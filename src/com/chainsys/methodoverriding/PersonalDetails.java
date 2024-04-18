27d61250fff5d3d9b72e20d1d9af71bfee06238b
Hari3552 <Hari3552@CSL0695.global.chainsys.com>
2024-04-18 10:09:17 +0530

    Method Overriding

--- src/com/chainsys/methodoverriding/PersonalDetails.java
+++ src/com/chainsys/methodoverriding/PersonalDetails.java
@@ -1,5 +1,7 @@
 package com.chainsys.methodoverriding;
 
+import java.util.Scanner;
+
 class Person {
 	public String name;
 	public int age;
@@ -8,7 +10,7 @@ class Person {
 		this.name = name;
 		this.age = age;
 	}
-	public void displayDetails() {
+	public void displayDetails() {		
 		System.out.println("Name: " + name);
 		System.out.println("Age: " + age);
 	}
@@ -16,7 +18,7 @@ class Person {
 
 class Student extends Person {
 	public int studentId;
-	
+
 	public Student(String name, int age, int studentId) {
 		super(name, age);
 		this.studentId = studentId;
@@ -45,12 +47,47 @@ class Teacher extends Person {
 
 public class PersonalDetails {
 	public static void main(String[] args) {
-		
-		Student student = new Student("John Doe", 20, 1001);
+		Scanner scanner = new Scanner(System.in);
+        String name = "";
+        int age = 0,studentID=0;
+        while (name.isEmpty()) {
+            System.out.print("Enter name: ");
+            name = scanner.nextLine().trim();
+            if (name.isEmpty()) {
+                System.out.println("Name cannot be empty. Please enter a valid name.");
+            }
+        }
+        while (age <= 0) {
+            System.out.print("Enter age: ");
+            try {
+                age = scanner.nextInt();
+                if (age <=0) {
+                    System.out.println("Age must be a positive integer. Please enter a valid age.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Age must be a positive integer. Please enter a valid age.");
+            }
+        }
+        while (studentID <= 0) {
+            System.out.print("Enter StudentID: ");
+            try {
+            	studentID = scanner.nextInt();
+                if (studentID <=0) {
+                    System.out.println("StudentID must be a positive integer. Please enter a valid StudentID.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. StudentID must be a positive integer. Please enter a valid StudentID.");
+            }
+        }
+
+
+		Student student = new Student("name", age, studentID);
 		Teacher teacher = new Teacher("Jane Smith", 35, "Mathematics");
 		System.out.println("Student Details:");
 		student.displayDetails();
+		System.out.println();
 		System.out.println("***********************");
+		System.out.println();
 		System.out.println("Teacher Details:");
 		teacher.displayDetails();
 	}
